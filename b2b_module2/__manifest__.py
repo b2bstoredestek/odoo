0ce7fdac4674392942ca7d1f0a6deefaa6abb288
b2bstoredestek <support@b2bstore.com>
2026-03-30 11:22:27 +0300

    Update __manifest__.py

--- b2b_module2/__manifest__.py
+++ b2b_module2/__manifest__.py
@@ -2,6 +2,9 @@
     'name': 'B2B Store Integration',
     'version': '1.0',
     'depends': ['base'],
+    'summary': 'B2B Store Integration for Odoo',
+    'author': 'B2BStore Destek',
     'data': [],
     'installable': True,
+    'application': True
 }
